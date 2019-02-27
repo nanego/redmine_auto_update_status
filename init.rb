b7331c9a4043bd331d2e3148a243228dc247caef
Vincent Robert <vincent.robert@nanego.com>
2019-02-27 14:48:10 +0100

    Init new plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_auto_update_status do
+  name 'Redmine Auto Update Status plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
