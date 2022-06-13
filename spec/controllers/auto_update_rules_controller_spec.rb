abab3fee4726f94c90f9fdf8fc974f4004b58f27
Julie TILATTI <j.tilatti@adacis.net>
2022-06-13 11:23:13 +0200

    Test not working

--- spec/controllers/auto_update_rules_controller_spec.rb
+++ spec/controllers/auto_update_rules_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe AutoUpdateRulesController, :type => :controller do
 
   render_views
 
-  fixtures :users, :auto_update_rules
+  fixtures :users, :auto_update_rules, :projects
 
   before do
     User.current = User.find(1)
@@ -30,6 +30,52 @@ RSpec.describe AutoUpdateRulesController, :type => :controller do
 
       expect(response).to redirect_to(auto_update_rules_path)
     end
+
+  end
+
+  context "POST copy" do
+    # TODO :
+    # créer des nouvelles règles dans /home/julie/Documents/redmine/redmine/plugins/redmine_auto_update_status/spec/fixtures/auto_update_rules.yml
+    # créer de nouvelles relations règles/projets dans /home/julie/Documents/redmine/redmine/plugins/redmine_auto_update_status/spec/fixtures/auto_update_rule_project.yml
+    # PENSER A REMETTRE LES BONNES FIXTURES DANS LE CORE POUR LES TESTS
+    # Tester que la table a bien été incrémentée de 1
+    # Tester l'auteur
+    # Le nom du projet --> attention à la langue
+    # tester que les projets ait bien été copié
+
+    let!(:rule_to_copy) { AutoUpdateRule.find(4) }
+
+    it "Copy an auto-update rule without changes" do
+        expect do
+          post :copy, :params => {
+            :id => rule_to_copy.id,
+            :auto_update_rules => {
+              :name => "test"
+            }
+
+          }
+        end.to change { AutoUpdateRule.count }.by(1)
+
+
+    end
+
+    it "Copy an auto-update rule with changes" do
+      expect(true).to eq(true)
+    end
+
+    # exemple
+    # field = ProjectCustomField.first
+    #       expect do
+    #         patch :update, :params => {
+    #           :id => field.id,
+    #           :custom_field => {
+    #             :name => "new_name",
+    #             :description => "new_des",
+    #             :is_required => true,
+    #           }
+    #         }
+    #       end.to change { JournalSetting.count }.by(1)
+    # expect(JournalSetting.last.value_changes).to include({ "name" => [field.name, "new_name"] })
 
   end
 
