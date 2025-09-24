e03c3c92f4e9bcf53d6f236f6826ae813f907b5e
Vincent <contact@vincent-robert.com>
2025-09-24 16:58:39 +0200

    Update CI config: add latest Redmine versions

--- db/migrate/20250701160618_add_missing_indexes.rb
+++ db/migrate/20250701160618_add_missing_indexes.rb
@@ -1,4 +1,4 @@
-class AddMissingIndexes < ActiveRecord::Migration[7.2]
+class AddMissingIndexes < ActiveRecord::Migration[6.1]
   def change
     add_index :auto_update_rules, :author_id, if_not_exists: true
     add_index :auto_update_rules, :project_id, if_not_exists: true
